fb863327e1bba0f4f9ada308deac6d4c62dcfce0
RahmitulHasanah <117124873+RahmitulHasanah@users.noreply.github.com>
2022-11-17 01:19:31 +0700

    Add files via upload

---
+++ latihan1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul_3;
+
+/**
+ *
+ * @author nilaenjeli
+ */
+public class latihan1 {
+    public static void main(String[] args) {
+        System.out.println("Nila Enjeli\n" + "2101092012\n" + "Manajemen Informatika\n" + "Teknologi Informasi\n" + "Kapalo Koto, Padang");
+        System.out.println("Nama\t: Nila\n" + "NoBp\t: 2101092012\n" + "Prodi\t: Manajemen Informatika\n" + "Alamat\t: Kapalo Koto, Padang\n");
+    }
+}
